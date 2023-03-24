0f457d1dd33c4f90db1ee9c31911591a8bc02633
frkngny <53319214+frkngny@users.noreply.github.com>
2023-03-24 18:00:57 +0300

    Delete FinalizeContracts.py

--- FinalizeContracts.py
+++
@@ -1,6 +0,0 @@
-from creds.keys import crypto_arsenal_payload
-from CryptoArsenal import CryptoArsenal
-
-crypto = CryptoArsenal()
-#Long:  {'times': [], 'prices': [26.3459, 26.3134, 26.291400000000003, 26.311400000000003, 26.295, 26.306700000000003, 26.312900000000003, 26.3723, 26.3643, 26.3564, 26.3343, 26.3413, 26.341400000000004, 26.3435, 26.358700000000002, 26.3401, 26.343799999999998, 26.3385, 26.3353]}
-print(crypto.send_webhook_request("close_long", "1679076879549.0"))
